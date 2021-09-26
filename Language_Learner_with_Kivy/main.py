6241c30961982d9d7ad75ea5490f43c9d2a5de05
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-27 00:16:12 +0200

    kv file MUST be named as the name of the class in lowercase minus "App"

--- Language_Learner_with_Kivy/main.py
+++ Language_Learner_with_Kivy/main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.button import Button
-
+# kv file MUST be named as the name of the class in lowercase minus "App"
 class LanguageLearnerApp(App): # A  subclass of type XxxXxxAPP is mandatory with Kivy
     def build(self): 
         return Button(text="Hello World") # We create a widget
