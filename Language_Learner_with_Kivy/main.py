b4671275896e2f45b10e44c9debd13197fac3f78
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-27 01:05:18 +0200

    class FunkyButton creation

--- Language_Learner_with_Kivy/main.py
+++ Language_Learner_with_Kivy/main.py
@@ -3,17 +3,21 @@ from kivy.uix.button import Button
 # kv file MUST be named as the name of the class in lowercase minus "App"
 
 class FunkyButton(Button):
-    pass
-
+    def __init__(self, **kwargs):
+        super(FunkyButton, self).__init__(**kwargs)
+        self.text="Funky Button", 
+        self.pos=(100,100),    
+        self.size_hint=(.25,.25 )
+        
 class LanguageLearnerApp(App): # A  subclass of type XxxXxxAPP is mandatory with Kivy
     def build(self): 
-        return FunkyButton(
-            text="Funky Button",
-            pos=(100,100),
-            # size=(100,100), #size seems to have benn modified from version 1,11,1 
-            # size_hint=(None,None)
-            size_hint=(.5,.5 ) # Responsive design !!!
-            ) # We create a widget -- positioning in kivy start from bottom left !!!
+        return FunkyButton()
+        #     text="Funky Button", 
+        #     pos=(100,100),
+        #     # size=(100,100), #size seems to have benn modified from version 1,11,1 
+        #     # size_hint=(None,None)
+        #     size_hint=(.5,.5 ) # Responsive design !!!
+        #     ) # We create a widget -- positioning in kivy start from bottom left !!!
 
 
 if __name__ == '__main__':
