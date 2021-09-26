1ab352b0da36f508834a8e0210364c78cd507557
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-27 00:24:04 +0200

    positioning in kivy start from bottom left !!!

--- Language_Learner_with_Kivy/main.py
+++ Language_Learner_with_Kivy/main.py
@@ -3,7 +3,9 @@ from kivy.uix.button import Button
 # kv file MUST be named as the name of the class in lowercase minus "App"
 class LanguageLearnerApp(App): # A  subclass of type XxxXxxAPP is mandatory with Kivy
     def build(self): 
-        return Button(text="Hello World") # We create a widget
+        return Button(
+            text="Hello World"
+            pos=(50, 50)) # We create a widget -- positioning in kivy start from bottom left !!!
 
 
 if __name__ == '__main__':
