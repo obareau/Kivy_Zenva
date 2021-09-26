bf5244ad92dd023f20bf877f7cd9208bd3c9c4af
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-27 01:06:56 +0200

    We don't needs comma anymore it's no longer a list of values

--- Language_Learner_with_Kivy/main.py
+++ Language_Learner_with_Kivy/main.py
@@ -5,8 +5,8 @@ from kivy.uix.button import Button
 class FunkyButton(Button):
     def __init__(self, **kwargs):
         super(FunkyButton, self).__init__(**kwargs)
-        self.text="Funky Button", 
-        self.pos=(100,100),    
+        self.text="Funky Button" 
+        self.pos=(100,100) # We don't needs comma anymore it's no longer a list of values    
         self.size_hint=(.25,.25 )
         
 class LanguageLearnerApp(App): # A  subclass of type XxxXxxAPP is mandatory with Kivy
